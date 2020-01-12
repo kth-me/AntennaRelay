5ac0fc40848b486e394fbe54f0063daec6e9acd7
Benjamin Hook <ben@benjaminhook.dev>
2020-01-12 14:26:52 +1000

    Test

--- tools/create-release.bat
+++ tools/create-release.bat
@@ -8,20 +8,18 @@ cd /d "..\src\%repoName%.%startUpProject%"
 dotnet run --configuration Release
 goto BeginProcess
 
-
-
 ::Creating a release branch
-git checkout -b release-1.2.0 develop
+git checkout -b release-1.1.2 develop
 bump-version minor
-git commit -a -m "Prepare release v1.2.0"
+git commit -a -m "Prepare release v1.1.2
 
 
 ::Finishing a release branch
 git checkout master
-git merge --no-ff release-1.2.0
-git tag -a v1.2.0 -m "Release v1.2.0"
+git merge --no-ff release-1.1.2
+git tag -a v1.1.2 -m "Release v1.1.2"
 git push origin master
 git push --tags
 
 git checkout develop
-git merge --no-ff release-1.2.0
+git merge --no-ff master
