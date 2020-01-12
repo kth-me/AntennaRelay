57c48eaf8c2dcd1df0f141d16159f23ff83115b7
Benjamin Hook <ben@benjaminhook.dev>
2020-01-12 14:19:00 +1000

    Revert release v1.1.2

--- tools/create-release.bat
+++ tools/create-release.bat
@@ -17,10 +17,11 @@ git commit -a -m "Prepare release v1.2.0"
 
 
 ::Finishing a release branch
-git checkout develop
-git merge --no-ff release-1.2.0
-
 git checkout master
 git merge --no-ff release-1.2.0
-git tag -a v1.2.0
+git tag -a v1.2.0 -m "Release v1.2.0"
+git push origin master
 git push --tags
+
+git checkout develop
+git merge --no-ff release-1.2.0
