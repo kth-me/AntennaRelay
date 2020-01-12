1d9623eab4453f6d1610eaac1bd3b8da819c94b2
Benjamin Hook <ben@benjaminhook.dev>
2020-01-12 17:34:56 +1000

    Bump version to 1.1.2

--- tools/create-release.bat
+++ tools/create-release.bat
@@ -1,27 +1,64 @@
 @echo off
 
-for %%G in ("%~dp0\..") do set repoName=%%~nxG
-set startUpProject="ConsoleApp"
-
 :BeginProcess
-cd /d "..\src\%repoName%.%startUpProject%"
-dotnet run --configuration Release
-goto BeginProcess
+cd ..
 
+:: Generate new VERSION file if one doesn't exist
+if exist %cd%\VERSION goto SetVariables
 
+set currentMajor=0
+set currentMinor=0
+set currentPatch=0
+(
+    echo %currentMajor%.%currentMinor%.%currentPatch%
+)>VERSION
 
-::Creating a release branch
-git checkout -b release-1.2.0 develop
-bump-version minor
-git commit -a -m "Prepare release v1.2.0"
+:: Set variables based on contents of VERSION file
+:SetVariables
+for /f "usebackq tokens=1,2,3 delims=." %%A in ("VERSION") do (
+  set currentMajor=%%A
+  set currentMinor=%%B
+  set currentPatch=%%C
+)
 
+:: Check for valid parameter (none defaults to patch) and bump version accordingly
+:ParamaterChecks
+if [%1]==[] goto BumpPatch
+if %1%==major goto BumpMajor
+if %1%==minor goto BumpMinor
+if %1%==patch goto BumpPatch
+goto Fail
 
-::Finishing a release branch
-git checkout master
-git merge --no-ff release-1.2.0
-git tag -a v1.2.0 -m "Release v1.2.0"
-git push origin master
-git push --tags
+:BumpMajor
+set /a newMajor=currentMajor+1
+set /a newMinor=0
+set /a newPatch=0
+goto CreateRelease
 
-git checkout develop
-git merge --no-ff release-1.2.0
+:BumpMinor
+set /a newMajor=currentMajor
+set /a newMinor=currentMinor+1
+set /a newPatch=0
+goto CreateRelease
+
+:BumpPatch
+set /a newMajor=currentMajor
+set /a newMinor=currentMinor
+set /a newPatch=currentPatch+1
+goto CreateRelease
+
+:: Create release branch, save new version in VERSION file, 
+:: set version based on contents of VERSION file to all 
+:: project files in lower directories (dependency: https://github.com/TAGC/dotnet-setversion)
+:CreateRelease
+set newVersion=%newMajor%.%newMinor%.%newPatch%
+git checkout -b release-%newVersion% develop
+(
+    echo %newVersion%
+)>VERSION
+setversion -r @VERSION
+git commit -a -m "Bump version to %newVersion%"
+goto EndProcess
+
+:EndProcess
+cd tools
